93fac3c53123d1e93db7c8c6d9fb2c2f3c62c643
赵明 <ming.zhao@karakal.com.cn>
2018-03-20 16:29:48 +0800

    1.简单工厂模式去掉junit

--- src/com/mingzhao/designpattern/pattern23/p6/simplefactory/after/SimpleFactoryTest.java
+++ src/com/mingzhao/designpattern/pattern23/p6/simplefactory/after/SimpleFactoryTest.java
@@ -1,25 +1,14 @@
 package com.mingzhao.designpattern.pattern23.p6.simplefactory.after;
 
-import static org.junit.Assert.*;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import com.mingzhao.designpattern.util.XMLUtil;
 
 public class SimpleFactoryTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Test
-	public void test() {
+	public static void main(String[] args) {
 		//通過配置文件工具類，這樣不用每次修改客戶端
 		String type = XMLUtil.getType();
 		Chart chart;
 		chart = ChartFactory.getChart(type);
-		assertEquals("PieChart", chart.getClass().getName());
 		chart.display();
 	}
 
